ef11d8c19098cb6483b77985797a01528f31140d
Matheusdn <matheusmartinsdn@gmail.com>
2020-04-22 22:27:55 -0300

    Initial project

---
+++ src/pages/Home/index.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+
+import api from '../../services/api';
+
+import * as CartActions from '../../store/modules/Cart/actions';
+
+import { ProductList } from './styles';
+
+class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  handleAddProduct = (id) => {
+    const { addToCartRequest } = this.props;
+
+    addToCartRequest(id);
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map((product) => ({
+      ...product,
+      priceFormatted: formatPrice(product.price),
+    }));
+
+    this.setState({
+      products: data,
+    });
+  }
+
+  render() {
+    const { products } = this.state;
+    const { amount } = this.props;
+
+    return (
+      <ProductList>
+        {products.map((product) => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <p>{product.priceFormatted}</p>
+
+            <button
+              type="button"
+              onClick={() => this.handleAddProduct(product.id)}
+            >
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  amount: state.Cart.reduce((amount, product) => {
+    amount[product.id] = product.amount;
+
+    return amount;
+  }, {}),
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
---
+++ src/store/modules/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import Cart from './Cart/reducer';
+
+export default combineReducers({
+  Cart,
+});
